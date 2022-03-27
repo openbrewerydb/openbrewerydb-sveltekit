64039b1d7e7d71f0adfbc57aa440ffc5497e0342
Chris J Mears <chris.mears@gmail.com>
2022-03-26 22:25:26 -0700

    chore:

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,80 @@
-import preprocess from 'svelte-preprocess';
-import tailwind from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: {
-				plugins: [
-					tailwind,
-					autoprefixer
-				]
-			}
-		})
-	],
+	// options passed to svelte.compile (https://svelte.dev/docs#compile-time-svelte-compile)
+	compilerOptions: {},
+
+	// an array of file extensions that should be treated as Svelte components
+	extensions: ['.svelte'],
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		adapter: undefined,
+		amp: false,
+		appDir: '_app',
+		browser: {
+			hydrate: true,
+			router: true
+		},
+		csp: {
+			mode: 'auto',
+			directives: {
+				'default-src': undefined
+				// ...
+			}
+		},
+		endpointExtensions: ['.js', '.ts'],
+		files: {
+			assets: 'static',
+			hooks: 'src/hooks',
+			lib: 'src/lib',
+			params: 'src/params',
+			routes: 'src/routes',
+			serviceWorker: 'src/service-worker',
+			template: 'src/app.html'
+		},
+		floc: false,
+		inlineStyleThreshold: 0,
+		methodOverride: {
+			parameter: '_method',
+			allowed: []
+		},
+		outDir: '.svelte-kit',
+		package: {
+			dir: 'package',
+			emitTypes: true,
+			// excludes all .d.ts and files starting with _ as the name
+			exports: (filepath) => !/^_|\/_|\.d\.ts$/.test(filepath),
+			files: () => true
+		},
+		paths: {
+			assets: '',
+			base: ''
+		},
+		prerender: {
+			concurrency: 1,
+			crawl: true,
+			default: false,
+			enabled: true,
+			entries: ['*'],
+			onError: 'fail'
+		},
+		routes: (filepath) => !/(?:(?:^_|\/_)|(?:^\.|\/\.)(?!well-known))/.test(filepath),
+		serviceWorker: {
+			register: true,
+			files: (filepath) => !/\.DS_Store/.test(filepath)
+		},
+		trailingSlash: 'never',
+		version: {
+			name: Date.now().toString(),
+			pollInterval: 0
+		},
+		vite: () => ({})
+	},
+
+	// SvelteKit uses vite-plugin-svelte. Its options can be provided directly here.
+	// See the available options at https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/config.md
+
+	// options passed to svelte.preprocess (https://svelte.dev/docs#compile-time-svelte-preprocess)
+	preprocess: null
 };
 
 export default config;
-
-// Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
-// const mode = process.env.NODE_ENV;
-// const dev = mode === 'development';
-// process.env.TAILWIND_MODE = dev ? 'watch' : 'build';
