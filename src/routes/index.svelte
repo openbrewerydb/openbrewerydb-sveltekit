670ecf48ae3f8751809ca314338ce0eb6f733188
Chris J Mears <chris.mears@gmail.com>
2022-05-01 21:23:54 -0700

    chore: Reset prettier config

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,57 +1,64 @@
 <script lang="ts">
-	import logo from '../../static/obdb-logo.png';
+  import logo from "../../static/obdb-logo.png";
 </script>
 
 <div class="text-center">
-	<div class="w-full sm:w-96 mx-auto my-6">
-		<img class="" src={logo} alt="Open Brewery DB Logo" />
-	</div>
-	<h1 class="text-lg tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-		<span class="block xl:inline">Free and open-source</span>
-		<span class="block text-yellow-600 xl:inline">brewery data</span>
-	</h1>
-	<p
-		class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
-	>
-		Open Brewery DB is a free dataset and API with public information on breweries, cideries,
-		brewpubs, and bottleshops.
-	</p>
-	<div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
-		<div class="rounded-md shadow">
-			<a
-				href="/start"
-				class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 md:py-4 md:text-lg md:px-10"
-			>
-				Get started
-			</a>
-		</div>
-		<div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-			<a
-				href="/demo"
-				class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-yellow-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
-			>
-				Live demo
-			</a>
-		</div>
-	</div>
-	<div class="mt-12">
-		<h2 class="mt-6 tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl">
-			The Goal
-		</h2>
-		<p
-			class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-3 md:text-xl md:max-w-3xl"
-		>
-			The goal of Open Brewery DB is to maintain an open-source, community-driven dataset and
-			provide a public API.
-		</p>
-		<h2 class="mt-6 tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl">
-			The Mission
-		</h2>
-		<p
-			class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-3 md:text-xl md:max-w-3xl"
-		>
-			It is our belief that public information should be freely accessible for the betterment of the
-			beer community and the happiness of web developers and data analysts.
-		</p>
-	</div>
+  <div class="w-full sm:w-96 mx-auto my-6">
+    <img class="" src={logo} alt="Open Brewery DB Logo" />
+  </div>
+  <h1
+    class="text-lg tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+  >
+    <span class="block xl:inline">Free and open-source</span>
+    <span class="block text-yellow-600 xl:inline">brewery data</span>
+  </h1>
+  <p
+    class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+  >
+    Open Brewery DB is a free dataset and API with public information on
+    breweries, cideries, brewpubs, and bottleshops.
+  </p>
+  <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+    <div class="rounded-md shadow">
+      <a
+        href="/start"
+        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 md:py-4 md:text-lg md:px-10"
+      >
+        Get started
+      </a>
+    </div>
+    <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+      <a
+        href="/demo"
+        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-yellow-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+      >
+        Live demo
+      </a>
+    </div>
+  </div>
+  <div class="mt-12">
+    <h2
+      class="mt-6 tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl"
+    >
+      The Goal
+    </h2>
+    <p
+      class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-3 md:text-xl md:max-w-3xl"
+    >
+      The goal of Open Brewery DB is to maintain an open-source,
+      community-driven dataset and provide a public API.
+    </p>
+    <h2
+      class="mt-6 tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl"
+    >
+      The Mission
+    </h2>
+    <p
+      class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-3 md:text-xl md:max-w-3xl"
+    >
+      It is our belief that public information should be freely accessible for
+      the betterment of the beer community and the happiness of web developers
+      and data analysts.
+    </p>
+  </div>
 </div>
