b17d4424630b1596c2b14c886446256693f8f435
Chris J Mears <chris.mears@gmail.com>
2022-05-04 21:36:49 -0700

    feat: Clarify homepage message

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import logo from "../../static/obdb-logo.png";
+  import logo from "../../static/obdb-logo.png"
 </script>
 
 <div class="text-center">
@@ -7,7 +7,7 @@
     <img class="" src={logo} alt="Open Brewery DB Logo" />
   </div>
   <h1
-    class="text-lg tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+    class="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
   >
     <span class="block xl:inline">Free and open-source</span>
     <span class="block text-yellow-600 xl:inline">brewery data</span>
@@ -46,7 +46,8 @@
       class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-3 md:text-xl md:max-w-3xl"
     >
       The goal of Open Brewery DB is to maintain an open-source,
-      community-driven dataset and provide a public API.
+      community-driven dataset and provide a public API for brewery-related
+      data.
     </p>
     <h2
       class="mt-6 tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl"
@@ -57,8 +58,8 @@
       class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-3 md:text-xl md:max-w-3xl"
     >
       It is our belief that public information should be freely accessible for
-      the betterment of the beer community and the happiness of web developers
-      and data analysts.
+      the betterment of the community and the happiness of web developers and
+      data analysts.
     </p>
   </div>
 </div>
