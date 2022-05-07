9f52b3a072fcd673197469012bf88da6404f5bd0
Chris J Mears <chris.mears@gmail.com>
2022-05-06 20:46:11 -0700

    feat: Add page MDSveX layout

--- src/routes/page.svelte
+++ src/routes/page.svelte
@@ -1,3 +1,19 @@
-<div class="markdown">
-  <slot />
-</div>
+<script>
+  // These props get filled in from the page's front matter
+  export let title
+  export let coverImageUrl
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta property="og:title" content={title} />
+  <meta property="og:image" content={coverImageUrl} />
+</svelte:head>
+
+<article class="markdown">
+  <h1>{title}</h1>
+  <div>
+    <slot />
+  </div>
+  <a href="/blog">Back to articles</a>
+</article>
