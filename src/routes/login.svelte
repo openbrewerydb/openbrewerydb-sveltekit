5fbd5c5141e7b097647352e009dc6935c209b22f
Chris J Mears <chris.mears@gmail.com>
2022-05-07 21:18:28 -0700

    feat: Update Login page

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -4,4 +4,10 @@
   <meta property="og:image" content="/obdb-og.png" />
 </svelte:head>
 
-<h1>Login</h1>
+<h1 class="text-4xl">Coming Soon!</h1>
+
+<p class="mt-4">Would you like to help with the moderation of OBDB data?</p>
+
+<div class="mt-4">
+  <a class="underline text-amber-600 hover:text-amber-900" href="mailto:info@openbrewerydb.org">Email Me</a>
+</div>
