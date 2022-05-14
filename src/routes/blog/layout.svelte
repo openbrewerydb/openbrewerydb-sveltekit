eaa2444c2778224310a9bfc7331cbb874bf26dc7
Chris J Mears <chris.mears@gmail.com>
2022-05-13 23:10:57 -0700

    feat: Better open graph and twitter card metadata

--- src/routes/blog/layout.svelte
+++ src/routes/blog/layout.svelte
@@ -6,9 +6,17 @@
 
 <svelte:head>
   <title>{title}</title>
-  <meta property="og:title" content="Open Brewery DB | {title}" />
+
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
   <meta property="og:image" content={coverImageUrl} />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@openbrewerydb" />
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={description} />
+  <meta name="twitter:image" content={coverImageUrl} />
 </svelte:head>
 
 <article>
--- src/routes/page.svelte
+++ src/routes/page.svelte
@@ -1,7 +1,7 @@
 <script>
   export let title;
   export let description = 'A worldwide open-source brewery dataset and API';
-  export let coverImageUrl = '/obdb-og.png';
+  export let coverImageUrl = 'https://www.openbrewerydb.org/obdb-og.png';
 </script>
 
 <svelte:head>
@@ -10,8 +10,17 @@
     property="og:title"
     content="Open Brewery DB{title ? ` | ${title}` : ''}"
   />
-  <meta property="og:image" content={coverImageUrl} />
+
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
+  <meta property="og:image" content={coverImageUrl} />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@openbrewerydb" />
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={description} />
+  <meta name="twitter:image" content={coverImageUrl} />
 </svelte:head>
 
 <article class="markdown">
