06ea0b10b801aa8d068e6291844719707c6123cc
Chris J Mears <chris.mears@gmail.com>
2022-05-13 22:35:57 -0700

    feat: Redirect /documentation/* to /documentation

---
+++ src/routes/documentation/[slug].svelte
@@ -0,0 +1,9 @@
+<script context="module">
+  /** @type {import('./[slug]').Load} */
+  export async function load() {
+    return {
+      status: 301,
+      redirect: '/documentation',
+    };
+  }
+</script>
