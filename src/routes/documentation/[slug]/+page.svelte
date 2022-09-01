94d1bb7ee2e3f5b52687af157fe558c5f47d9889
Chris J Mears <chris.mears@gmail.com>
2022-08-31 22:12:29 -0700

    chore: Upgrade SvelteKit
    This is a big breaking change which
    required updates to the file structure.

---
+++ src/routes/documentation/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+  /** @type {import('../../../../.svelte-kit/types/src/routes/documentation./../../../.svelte-kit/types/src/routes/documentation/[slug]').Load} */
+  export async function load() {
+    return {
+      status: 301,
+      redirect: '/documentation',
+    };
+  }
+</script>
