0ac79e6782d5e9a23d5341a6b710f563497d7a03
Chris J Mears <chris.mears@gmail.com>
2022-11-05 21:02:03 -0700

    fix: Documentation

--- src/routes/documentation/[slug]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script context="module">
-  /** @type {import('../../../../../.svelte-kit/types/src/routes/documentation/[slug]/$types').Load} */
-  export async function load() {
-    return {
-      status: 301,
-      redirect: '/documentation',
-    };
-  }
-</script>
