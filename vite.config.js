f1ce16ad55e90438132fb6e0e5f5568dc867b66a
Chris Mears <chris.mears@gmail.com>
2025-04-19 20:43:49 -0700

    [Feature] Add Sentry error monitoring and reorganize gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [sveltekit(), tailwindcss()],
+  plugins: [
+    sentrySvelteKit({
+      sourceMapsUploadOptions: {
+        org: 'open-brewery-db',
+        project: 'obdb-sveltekit',
+      },
+    }),
+    sveltekit(),
+    tailwindcss(),
+  ],
 });
