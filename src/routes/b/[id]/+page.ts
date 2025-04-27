0072e90ea6a99ba797e750f0235c3104cef8f3d9
Chris Mears <chris.mears@gmail.com>
2025-04-26 20:56:28 -0700

    [Fix] Add error handling for brewery details page and fix string interpolation (Issue #38)

--- src/routes/b/[id]/+page.ts
+++ src/routes/b/[id]/+page.ts
@@ -1,11 +1,12 @@
 import type { Brewery } from '$lib/types';
 import { API_URL } from '$lib/utils';
-
-/** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
   const { id }: { id: string } = params;
 
   const breweryResults = await fetch(`${API_URL}/breweries/${id}`);
-  const brewery: Brewery = await breweryResults.json();
-  return { brewery };
+  let brewery: Brewery | null = null;
+  if (breweryResults.ok) {
+    brewery = await breweryResults.json();
+  }
+  return { brewery, id };
 }
